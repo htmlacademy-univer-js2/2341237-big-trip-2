034d2503cb6c3aa4b6e769480fa9772a42d3150b
DeadVafel <0202052003@mail.ru>
2023-06-16 13:16:41 +0500

    Пришёл, увидел, загрузил (часть 2)

--- src/model/point-model.js
+++ src/model/point-model.js
@@ -1,31 +1,47 @@
 import Observable from '../framework/observable.js';
 import { UpdateType } from '../const.js';
+import dayjs from 'dayjs';
 
 export default class PointsModel extends Observable{
-  #points = [];
   #pointsApiService = null;
+  #points = [];
+  #offers = [];
+  #destinations = [];
 
   constructor(pointsApiService) {
     super();
     this.#pointsApiService = pointsApiService;
   }
 
+  get points () {
+    return this.#points;
+  }
+
+  get offers () {
+    return this.#offers;
+  }
+
+  get destinations () {
+    return this.#destinations;
+  }
+
   init = async () => {
     try {
       const points = await this.#pointsApiService.points;
       this.#points = points.map(this.#adaptToClient);
+      this.#offers = await this.#pointsApiService.offers;
+      this.#destinations = await this.#pointsApiService.destinations;
     } catch(err) {
       this.#points = [];
+      this.#offers = [];
+      this.#destinations = [];
     }
 
     this._notify(UpdateType.INIT);
   };
 
-  get points() {
-    return this.#points;
-  }
-
   updatePoint = async (updateType, update) => {
+
     const index = this.#points.findIndex((point) => point.id === update.id);
 
     if (index === -1) {
@@ -35,47 +51,65 @@ export default class PointsModel extends Observable{
     try {
       const response = await this.#pointsApiService.updatePoint(update);
       const updatedPoint = this.#adaptToClient(response);
+
       this.#points = [
         ...this.#points.slice(0, index),
         updatedPoint,
         ...this.#points.slice(index + 1),
       ];
+
       this._notify(updateType, updatedPoint);
+
     } catch(err) {
       throw new Error('Can\'t update point');
     }
   };
 
-  addPoint = (updateType, update) => {
-    this.#points.unshift(update);
+  addPoint = async (updateType, update) => {
 
-    this._notify(updateType, update);
+    try {
+      const response = await this.#pointsApiService.addPoint(update);
+      const newPoint = this.#adaptToClient(response);
+
+      this.#points = [
+        newPoint,
+        ...this.#points,
+      ];
+      this._notify(updateType, update);
+
+    } catch(err) {
+      throw new Error('Can\'t add point');
+    }
   };
 
-  deletePoint = (updateType, update) => {
+  deletePoint = async (updateType, update) => {
     const index = this.#points.findIndex((point) => point.id === update.id);
 
     if (index === -1) {
       throw new Error('Can\'t delete unexisting point');
     }
 
-    this.#points = [
-      ...this.#points.slice(0, index),
-      ...this.#points.slice(index + 1),
-    ];
-
-    this._notify(updateType);
+    try {
+      await this.#pointsApiService.deletePoint(update);
+      this.#points = [
+        ...this.#points.slice(0, index),
+        ...this.#points.slice(index + 1),
+      ];
+      this._notify(updateType);
+    } catch(err) {
+      throw new Error('Can\'t delete point');
+    }
   };
 
   #adaptToClient = (point) => {
+
     const adaptedPoint = {...point,
       basePrice: point['base_price'],
-      dateFrom: point['date_from'] !== null ? new Date(point['date_from']) : point['date_from'],
-      dateTo: point['date_to'] !== null ? new Date(point['date_to']) : point['date_to'],
+      startDate: dayjs(point['date_from']),
+      endDate: dayjs(point['date_to']),
       isFavorite: point['is_favorite'],
     };
 
-    // Ненужные ключи мы удаляем
     delete adaptedPoint['base_price'];
     delete adaptedPoint['date_from'];
     delete adaptedPoint['date_to'];
@@ -83,4 +117,5 @@ export default class PointsModel extends Observable{
 
     return adaptedPoint;
   };
+
 }
