e01aa031968a10f9b5a960f549e5ffa5240e1496
DeadVafel <0202052003@mail.ru>
2023-06-16 12:14:21 +0500

    Пришёл, увидел, загрузил (часть 1)

--- src/main.js
+++ src/main.js
@@ -1,28 +1,41 @@
 import { render } from './framework/render.js';
 import FilterPresenter from './presenter/filter-presenter.js';
 import BoardPresenter from './presenter/board-presenter.js';
-import SiteMenuView from './view/menu-view.js';
-import PointsModel from './model/point-model.js';
-import FilterModel from './model/filter-model.js';
-import NewPointButtonView from './view/new-point-view.js';
-import { getPoints, getDestinations, getOffersByType } from './mock/point.js';
+import SiteMenuView from './view/site-menu-view.js';
+import PointsModel from './model/points-model.js';
+import FilterModel from './model/filters-model.js';
+import NewPointButtonView from './view/new-point-button-view.js';
+import DestinationsModel from './model/destinations-model.js';
+import OffersModel from './model/offers-model.js';
+import PointsApiService from './api-service/point-api-service.js';
+import DestinationsApiService from './api-service/destination-api-service.js';
+import OffersApiService from './api-service/offer-api-service.js';
+
+const AUTHORIZATION = 'Basic hIfpbpd204fpubd6';
+const END_POINT = 'https://18.ecmascript.pages.academy/big-trip';
 
 const siteHeaderElement = document.querySelector('.trip-main');
 const siteMainElement = document.querySelector('.page-main');
 
-
-const points = getPoints();
-const offersByType = getOffersByType();
-const destinations = getDestinations();
-
-const pointsModel = new PointsModel();
-pointsModel.init(points, destinations, offersByType);
+const pointsModel = new PointsModel(new PointsApiService(END_POINT, AUTHORIZATION));
+const destinationsModel = new DestinationsModel(new DestinationsApiService(END_POINT, AUTHORIZATION));
+const offersModel = new OffersModel(new OffersApiService(END_POINT, AUTHORIZATION));
 
 const filterModel = new FilterModel();
-const filterPresenter = new FilterPresenter(siteHeaderElement.querySelector('.trip-controls__filters'), filterModel, pointsModel);
+const filterPresenter = new FilterPresenter({
+  filterContainer: siteHeaderElement.querySelector('.trip-controls__filters'),
+  pointsModel: pointsModel,
+  filterModel: filterModel
+});
 filterPresenter.init();
 
-const boardPresenter = new BoardPresenter(siteMainElement.querySelector('.trip-events'), pointsModel, filterModel);
+const boardPresenter = new BoardPresenter({
+  tripContainer: siteMainElement.querySelector('.trip-events'),
+  pointsModel: pointsModel,
+  filterModel: filterModel,
+  destinationsModel: destinationsModel,
+  offersModel: offersModel
+});
 boardPresenter.init();
 
 const newPointButtonComponent = new NewPointButtonView();
@@ -35,10 +48,15 @@ const handleNewPointButtonClick = () => {
   boardPresenter.createPoint(handleNewPointFormClose);
   newPointButtonComponent.element.disabled = true;
 };
-
-render(newPointButtonComponent, siteHeaderElement);
-newPointButtonComponent.setClickHandler(handleNewPointButtonClick);
-
+offersModel.init().finally(() => {
+  destinationsModel.init().finally(() => {
+    pointsModel.init().finally(() => {
+      render(newPointButtonComponent, siteHeaderElement);
+      newPointButtonComponent.setClickHandler(handleNewPointButtonClick);
+    });
+  });
+});
 
 render(new SiteMenuView(), siteHeaderElement.querySelector('.trip-controls__navigation'));
+
 
